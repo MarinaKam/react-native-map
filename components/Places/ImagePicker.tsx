d7feda3fab3fff85164138c8a44814c93520bb90
Maryna Kamysheva <marina.stozhok@gmail.com>
2024-04-18 16:58:36 -0700

    feat: Add the ability to take/choose images.

--- components/Places/ImagePicker.tsx
+++ components/Places/ImagePicker.tsx
@@ -1,6 +1,13 @@
 import { FC, useState } from 'react';
 import { Alert, Image, StyleSheet, View } from 'react-native';
-import { launchImageLibraryAsync, useCameraPermissions, PermissionStatus, MediaTypeOptions } from 'expo-image-picker';
+import {
+  PermissionStatus,
+  MediaTypeOptions,
+  launchCameraAsync,
+  useCameraPermissions,
+  launchImageLibraryAsync,
+  useMediaLibraryPermissions
+} from 'expo-image-picker';
 import { ImagePickerAsset } from 'expo-image-picker/src/ImagePicker.types';
 import { Text } from '../Text';
 import { useGlobalTheme } from '../../store';
@@ -9,6 +16,7 @@ import { Button } from '../Button';
 export const ImagePicker: FC = () => {
   const { paletteTheme } = useGlobalTheme();
   const [cameraPermissionInfo, requestPermission] = useCameraPermissions();
+  const [libraryPermissionInfo, requestLibraryPermission] = useMediaLibraryPermissions();
   const [pickedImage, setPickedImage] = useState<ImagePickerAsset | null>(null);
 
   const verifyPermission = async () => {
@@ -27,6 +35,20 @@ export const ImagePicker: FC = () => {
     return true;
   };
 
+  const verifyLibraryPermission = async () => {
+    if (libraryPermissionInfo && libraryPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionRes = await requestLibraryPermission();
+      return permissionRes.granted;
+    }
+
+    if (libraryPermissionInfo && libraryPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert('Insufficient Permissions!', 'You need to grant media library permissions to use this app.');
+      return false;
+    }
+
+    return true;
+  };
+
   const takeImageHandler = async () => {
     const hasPermission = await verifyPermission();
 
@@ -34,12 +56,30 @@ export const ImagePicker: FC = () => {
       return;
     }
     // TODO: this one doesn't work on ios emulator
-    // const image = await launchCameraAsync({
-    const image = await launchImageLibraryAsync({
-      mediaTypes: MediaTypeOptions.All,
+    const image = await launchCameraAsync({
+    // const image = await launchImageLibraryAsync({
+      mediaTypes: MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [16, 9],
-      quality: 1, // set to 0.5 if use camera and real device
+      quality: 0.5, // set to 0.5 if use camera and real device
+    });
+
+    if (!image.canceled) {
+      setPickedImage(image.assets[0]);
+    }
+  };
+
+  const pickImageHandler = async () => {
+    const hasPermission = await verifyLibraryPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchImageLibraryAsync({
+      mediaTypes: MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5
     });
 
     if (!image.canceled) {
@@ -57,9 +97,15 @@ export const ImagePicker: FC = () => {
         )}
       </View>
 
-      <Button mode="outlined" icon="camera" onPress={takeImageHandler}>
-        Take Image
-      </Button>
+      <View style={styles.buttonsGroup}>
+        <Button mode="contained" icon="image" onPress={pickImageHandler} style={[ styles.button, { marginRight: 4 } ]}>
+          Choose Image
+        </Button>
+
+        <Button mode="outlined" icon="camera" onPress={takeImageHandler} style={[ styles.button, { marginLeft: 4 } ]}>
+          Take Image
+        </Button>
+      </View>
     </View>
   );
 };
@@ -80,4 +126,12 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  buttonsGroup: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 16
+  },
+  button: {
+    flex: 1,
+  }
 });
