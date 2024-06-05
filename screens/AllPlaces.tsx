fff5ca250d5809d4ee7699c98bc85f86ef3979d6
Maryna Kamysheva <marina.stozhok@gmail.com>
2024-06-04 18:52:13 -0700

    feat: Add item view, fix navigation typescript errors.

--- screens/AllPlaces.tsx
+++ screens/AllPlaces.tsx
@@ -1,5 +1,23 @@
 import { PlacesList } from '../components';
+import { useIsFocused, useRoute, RouteProp } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { Place } from '../models/place';
+import { RootStackParamList } from '../navigation';
+
+type AllPlacesRoute = RouteProp<RootStackParamList, 'AllPlaces'>;
 
 export const AllPlaces = () => {
-  return <PlacesList />;
+  const route = useRoute<AllPlacesRoute>();
+  const isFocused = useIsFocused();
+  const [loadedPlaces, setLoadedPlaces] = useState<Place[]>([]);
+
+  useEffect(() => {
+    if (isFocused && route?.params?.place) {
+      if (!loadedPlaces.find((place) => place.id === route?.params?.place?.id)) {
+        setLoadedPlaces((curPlaces) => [...curPlaces, (route?.params?.place as Place)]);
+      }
+    }
+  }, [isFocused]);
+
+  return <PlacesList places={loadedPlaces} />;
 };
